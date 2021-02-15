d1d9e41a2bc067bc95ba75fe2f47eeddecb7a513
michile louis <969768574@qq.com>
2021-02-16 05:21:27 +0800

    Update atomicswap.js

--- modules/atomicswap.js
+++ modules/atomicswap.js
@@ -1,17 +1,8 @@
-/**
- * Atomic swap class
- *
- * @author Djenad Razic
- * @company Altcoin Exchange, Inc.
- */
+
 var AtomicSwap;
 AtomicSwap = function (configuration, appConfiguration) {
     this.engine = null;
 
-    /**
-     * @param configuration
-     * @param appConfiguration
-     */
     this.construct = function (configuration, appConfiguration) {
         var Engine = require("./engine");
         this.engine = new Engine(configuration, appConfiguration);
@@ -20,15 +11,6 @@ AtomicSwap = function (configuration, appConfiguration) {
 
     this.construct(configuration, appConfiguration);
 
-    /**
-     * Initiate atomic swap transfer
-     * @param refundTime
-     * @param secret - Secret hash
-     * @param address - Participant address
-     * @param amount - Amount to transfer
-     * @param extendedParams
-     * @constructor
-     */
     this.Initiate = function (refundTime, secret, address, amount, extendedParams) {
         var conversion = (extendedParams && extendedParams.conversion) ? extendedParams.conversion : 'milliether';
 
@@ -41,14 +23,6 @@ AtomicSwap = function (configuration, appConfiguration) {
         return this.engine.callFunction("initiate", [refundTime, secret, address], params);
     };
 
-    /**
-     * Participate to atomic swap transfer
-     * @param refundTime
-     * @param secret - Secret hash
-     * @param address - Participant address
-     * @param amount
-     * @param extendedParams
-     */
     this.Participate = function (refundTime, secret, address, amount, extendedParams) {
         var conversion = (extendedParams && extendedParams.conversion) ? extendedParams.conversion : 'milliether';
 
@@ -61,12 +35,6 @@ AtomicSwap = function (configuration, appConfiguration) {
         return this.engine.callFunction("participate", [refundTime, secret, address], params);
     };
 
-    /**
-     * Redeem funds with given secret
-     * @param secret - Secret hash
-     * @param hashedSecret
-     * @param extendedParams
-     */
     this.Redeem = function (secret, hashedSecret, extendedParams) {
 
         var params = {
@@ -77,11 +45,6 @@ AtomicSwap = function (configuration, appConfiguration) {
         return this.callFunction("redeem", [secret, hashedSecret], params);
     };
 
-    /**
-     * Refund contract transaction
-     * @param hashedSecret
-     * @param extendedParams
-     */
     this.Refund = function (hashedSecret, extendedParams) {
 
         var params = {
